408481d5dd5cbd633c0da27f1e2b277463f297c7
gio <kge91186@mail.ru>
2024-11-12 09:43:38 +0300

    pydentic models creation

--- fast_api/src/models/film.py
+++ fast_api/src/models/film.py
@@ -1,8 +1,34 @@
 
 # Используем pydantic для упрощения работы при перегонке данных из json в объекты
+from typing import List, Optional
 from pydantic import BaseModel
 
+
+class Person(BaseModel):
+    uuid:str
+    name:str
+    
+class Actor(Person):
+    pass
+
+class Writer(Person):
+    pass
+
+class Director(Person):
+    pass
+
+class Genre(BaseModel):
+    uuid:str
+    name:str
+    
 class Film(BaseModel):
-    id: str
+    uuid: str
     title: str
-    description: str
+    imdb_rating: Optional[float]
+    description:Optional[str]
+    genre:List[Genre]
+    actors:List[Actor]
+    writers:List[Writer]
+    directors:List[Director]
+    
+    
