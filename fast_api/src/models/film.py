cbfe1033458aed805c9376d1425a6ad820921246
suvorovjr <pglavan1998@mail.ru>
2024-11-20 21:50:08 +0300

    Add project models

--- fast_api/src/models/film.py
+++ fast_api/src/models/film.py
@@ -1,44 +1,31 @@
-
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
-from typing import List, Optional
-from uuid import UUID
 from pydantic import BaseModel
 
-        
-class ShortFilm(BaseModel):
-    id:str
-    title:str
-    imdb_rating:float
-
-class FilmRole(BaseModel):
-    id: str
-    roles: List[str]
 
 class Person(BaseModel):
-    id:str
-    full_name:str
-    films:List[FilmRole]
-
-class EntityBase(BaseModel):
     id: str
     name: str
-class Actor(EntityBase):
-    pass
-class Writer(EntityBase):
-    pass
-class Director(EntityBase):
-    pass
-class Genre(EntityBase):
-    pass
-    
+
+
 class Film(BaseModel):
     id: str
+    imdb_rating: float
+    genres: list[str]
     title: str
-    imdb_rating: Optional[float]
-    description:Optional[str]
-    genres:List[str]
-    actors:List[Actor]
-    writers:List[Writer]
-    directors:List[Director]
-    
-    
+    description: str | None = None
+    directors_names: str
+    actors_names: str
+    writers_names: str
+    directors: list[Person]
+    actors: list[Person]
+    writers: list[Person]
+
+
+class ResponseFilm(BaseModel):
+    id: str
+    imdb_rating: float
+    genres: list[str]
+    title: str
+    description: str | None = None
+    directors: list[Person]
+    actors: list[Person]
+    writers: list[Person]
