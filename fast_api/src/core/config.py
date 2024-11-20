0fe47fb12d0942c2b768ac86951032b1a0ea45a2
suvorovjr <pglavan1998@mail.ru>
2024-11-20 21:48:47 +0300

    Add new configs in project

--- fast_api/src/core/config.py
+++ fast_api/src/core/config.py
@@ -1,21 +1,40 @@
-import os
+from pathlib import Path
 from logging import config as logging_config
+
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 
 from src.core.logger import LOGGING
 
-# Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
-# Название проекта. Используется в Swagger-документации
-PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
+BASE_DIR = Path(__file__).parent.parent.parent
 
-# Настройки Redis
-REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+load_dotenv()
 
-# Настройки Elasticsearch
-ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
-ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
-# Корень проекта
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+class Settings(BaseSettings):
+    PROJECT_NAME: str = 'movies'
+    REDIS_HOST: str = '127.0.0.1'
+    REDIS_PORT: int = 6379
+    ELASTIC_HOST: str = '127.0.0.1'
+    ELASTIC_PORT: int = 9200
+    REDIS_PASSWORD: str
+
+    @property
+    def elastic_url(self) -> str:
+        """Формируем URL для подключения к Elasticsearch."""
+        return f'http://{self.ELASTIC_HOST}:{self.ELASTIC_PORT}'
+
+    @property
+    def redis_url(self) -> str:
+        """Формируем URL для подключения к Redis"""
+        return f'http://{self.REDIS_HOST}:{self.REDIS_PORT}'
+
+    class Config:
+        env_file_encoding = 'utf-8'
+        case_sensitive = True
+        extra = "allow"
+
+
+settings = Settings()
