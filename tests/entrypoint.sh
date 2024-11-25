033c9b1025c2ccb2db38ff7f16d1a330ebf5bcb2
suvorovjr <pglavan1998@mail.ru>
2024-11-25 19:49:13 +0300

    Add entrypoint in project

---
+++ tests/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Контейнер стартует"
+
+echo "Жду подключения к Redis..."
+python3 /code/tests/functional/utils/wait_for_redis.py
+echo "Redis успешно запущен!"
+
+echo "Жду подключения к Elasticsearch..."
+python3 /code/tests/functional/utils/wait_for_elastic.py
+echo "Elasticsearch успешно запущен!"
+
+echo "-------Запуск тестов-------"
+pytest /code/tests/functional/src"
