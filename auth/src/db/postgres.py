4fc8bdf12fce4c5e3c57991d1f178f33cd004894
suvorovjr <pglavan1998@mail.ru>
2024-12-23 01:00:55 +0300

    Add changes in postgres connection

--- auth/src/db/postgres.py
+++ auth/src/db/postgres.py
@@ -1,36 +1,26 @@
-from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import clear_mappers
 
+from src.models.adapters.orm import metadata, start_mappers
 from src.core.config import settings
 
-
-
-Base = declarative_base()
 
 engine = create_async_engine(settings.db.db_url, echo=True, future=True)
 
 
 async def get_session() -> AsyncSession:  # type: ignore
-    async_session = sessionmaker(
-        engine, class_=AsyncSession, expire_on_commit=False
-    )
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session
 
 
 async def create_database() -> None:
-    from src.models.users import User
-    from src.models.permissions import Permission
-    from src.models.sessions import Session
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(metadata.create_all)
+
+    start_mappers()
 
 
 async def purge_database() -> None:
-    from src.models.permissions import Permission
-    from src.models.users import User
-    from src.models.sessions import Session
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+    clear_mappers()
