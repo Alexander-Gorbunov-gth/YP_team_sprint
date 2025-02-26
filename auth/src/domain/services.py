ffbcbf134ff040b4370af6f5b5eebb3d9c1701d5
suvorovjr <pglavan1998@mail.ru>
2025-02-26 21:48:21 +0300

    Add auth service in project

--- auth/src/domain/services.py
+++ auth/src/domain/services.py
@@ -3,8 +3,11 @@ import logging
 from datetime import timedelta, datetime
 
 import jwt
+from passlib.context import CryptContext
 
 from src.domain.entities import User, Token
+from src.domain.exceptions import WrongOldPassword, WrongEmailOrPassword
+from src.domain.repositories import AbstractUserRepository
 
 logger = logging.getLogger(__name__)
 
@@ -69,9 +72,7 @@ class JWTService:
         :return: Сгенерированный refresh JWT токен в виде строки.
         """
 
-        return self._generate_token(
-            user=user, token_lifetime=self.refresh_token_lifetime
-        )
+        return self._generate_token(user=user, token_lifetime=self.refresh_token_lifetime)
 
     def decode_token(self, jwt_token: str) -> Token:
         """
@@ -93,3 +94,79 @@ class JWTService:
             logger.error("Ошибка декодирования токена %s", jwt_token)
             raise jwt.PyJWTError
         return token
+
+
+class AuthService:
+    def __init__(self, user_repository: AbstractUserRepository):
+        self._user_repository: AbstractUserRepository = user_repository
+        self._context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    async def registration_new_user(self, email: str, password: str) -> User:
+        """
+        Регистрирует нового пользователя, хешируя его пароль перед сохранением.
+
+        :param email: Email пользователя.
+        :param password: Открытый пароль пользователя.
+        :return: Созданный объект User.
+        """
+        
+        hashed_password = self._get_password_hash(password)
+        new_user = await self._user_repository.create(email=email, password=hashed_password)
+        return new_user
+
+    async def login_user(self, email: str, password: str) -> bool:
+        """
+        Проверяет учетные данные пользователя при входе.
+
+        :param email: Email пользователя.
+        :param password: Открытый пароль пользователя.
+        :return: True, если аутентификация успешна.
+        :raises WrongEmailOrPassword: Если учетные данные неверны.
+        """
+
+        user = await self._user_repository.get_by_email(email=email)
+        if user is None or not self._verify_password(password, user.password):
+            logger.error("Неверный логин для пользователя %s", email)
+            raise WrongEmailOrPassword
+        return True
+    
+    async def change_password(self, user_id: str, old_password: str, new_password: str) -> User:
+        """
+        Изменяет пароль пользователя.
+
+        :param user_id: Идентификатор пользователя.
+        :param old_password: Старый пароль пользователя.
+        :param new_password: Новый пароль пользователя.
+        :return: Обновленный объект User.
+        :raises WrongOldPassword: Если старый пароль неверен.
+        """
+
+        user = await self._user_repository.get_by_id(user_id=user_id)
+        if not self._verify_password(old_password, user.password):
+            logger.error("Неверный пароль для пользоавтеля %s.", str(user.id))
+            raise WrongOldPassword
+        hashed_new_password = self._get_password_hash(new_password)
+        user.password = hashed_new_password
+        updated_user = await self._user_repository.update(user=user)
+        return updated_user
+
+    def _get_password_hash(self, password) -> str:
+        """
+        Генерирует хеш пароля с использованием bcrypt.
+
+        :param password: Открытый пароль.
+        :return: Захешированный пароль.
+        """
+
+        return self._context.hash(password)
+
+    def _verify_password(self, password: str, hashed_password: str) -> bool:
+        """
+        Проверяет соответствие пароля и его хеша.
+
+        :param password: Открытый пароль.
+        :param hashed_password: Захешированный пароль.
+        :return: True, если пароль корректен.
+        """
+
+        return self._context.verify(password, hashed_password)
