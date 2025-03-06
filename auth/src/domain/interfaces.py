6737fade6365be063b2ca143d2a5aa872e518c9b
suvorovjr <pglavan1998@mail.ru>
2025-03-06 22:12:18 +0300

    Add interfaces in project

---
+++ auth/src/domain/interfaces.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+from src.domain.entities import User, Token
+
+
+class AbstractJWTService(ABC):
+    @abstractmethod
+    def generate_access_token(self, user: User) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate_refresh_token(self, user: User) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def decode_token(self, jwt_token: str) -> Token:
+        raise NotImplementedError
+
+
+class AbstractAuthToken(ABC):
+    @abstractmethod
+    async def registration_new_user(self, email: str, password: str) -> User:
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def login_user(self, email: str, password: str) -> bool:
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def change_password(self, user_id: str, old_password: str, new_password: str) -> User:
+        raise NotImplementedError
