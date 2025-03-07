79b6c41b4319e0d0b06debbc2619e2f8c234fab0
suvorovjr <pglavan1998@mail.ru>
2025-03-07 20:46:10 +0300

    Add abstract session repository

--- auth/src/domain/repositories.py
+++ auth/src/domain/repositories.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import timedelta
 
-from src.domain.entities import User
+from src.domain.entities import User, Session
 
 
 class AbstractUserRepository(ABC):
@@ -22,7 +22,25 @@ class AbstractUserRepository(ABC):
         raise NotImplementedError
 
 
-class BlackListRepository(ABC):
+class AbstractSessionRepository(ABC):
+    @abstractmethod
+    async def create(self, session: Session) -> Session:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_refresh_token(self, refresh_token: str) -> Session | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_other_sessions(self, refresh_token: str) -> list[Session]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update(self, session: Session) -> Session | None:
+        raise NotImplementedError
+
+
+class AbstractBlackListRepository(ABC):
     @abstractmethod
     async def exists(self, token: str) -> bool:
         raise NotImplementedError
