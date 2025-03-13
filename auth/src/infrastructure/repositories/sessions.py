8d64a560d7089b2cac78242cb555a9f38510a420
suvorovjr <pglavan1998@mail.ru>
2025-03-13 17:59:03 +0300

    Add changes in session repository

--- auth/src/infrastructure/repositories/sessions.py
+++ auth/src/infrastructure/repositories/sessions.py
@@ -1,30 +1,40 @@
 from uuid import UUID
 from dataclasses import asdict
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert, update, Result
 
+from src.db.postgres import get_session
 from src.domain.entities import Session
 from src.domain.repositories import AbstractSessionRepository
 
 
 class SQLAlchemySessionRepository(AbstractSessionRepository):
+    exclude_fields = ("id", "created_at", "updated_at")
+
     def __init__(self, session: AsyncSession):
         self._session: AsyncSession = session
 
     async def create(self, session: Session) -> Session:
-        query = insert(Session).values(asdict(session)).returning(Session)
+        query = (
+            insert(Session)
+            .values(session.to_dict(self.exclude_fields))
+            .returning(Session)
+        )
         result: Result = await self._session.execute(query)
+        await self._commit()
         return result.scalar_one()
 
     async def update(self, session: Session) -> Session | None:
         query = (
             update(Session)
             .filter_by(id=session.id)
-            .values(asdict(session))
+            .values(session.to_dict(self.exclude_fields))
             .returning(Session)
         )
         result: Result = await self._session.execute(query)
+        await self._commit()
         return result.scalar_one()
 
     async def get_by_refresh_token(self, refresh_token: str) -> Session | None:
@@ -36,3 +46,13 @@ class SQLAlchemySessionRepository(AbstractSessionRepository):
         query = select(Session).filter_by(user_id=user_id)
         result: Result = await self._session.execute(query)
         return result.scalars().all()
+
+    async def _commit(self):
+        await self._session.commit()
+
+
+def get_session_repository(
+    session: AsyncSession = Depends(get_session),
+) -> AbstractSessionRepository:
+    session_repository = SQLAlchemySessionRepository(session=session)
+    return session_repository
