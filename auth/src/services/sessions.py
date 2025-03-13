704b3b90fe94d4bac7c4778594d25d96b22dd9e3
suvorovjr <pglavan1998@mail.ru>
2025-03-13 18:02:23 +0300

    Add changes in session service and add dependencies

--- auth/src/services/sessions.py
+++ auth/src/services/sessions.py
@@ -1,21 +1,25 @@
+from fastapi import Depends
+
 from src.domain.entities import Session
 from src.domain.interfaces import AbstractSessionService
+from src.domain.repositories import AbstractSessionRepository
+from src.infrastructure.repositories.sessions import get_session_repository
 
 
 class SessionService(AbstractSessionService):
-    def __init__(self, session_repository):
-        self._session_repository = session_repository
+    def __init__(self, session_repository: AbstractSessionRepository):
+        self._session_repository: AbstractSessionRepository = session_repository
 
     async def create_new_session(self, session: Session) -> Session:
         new_session = await self._session_repository.create(session)
         return new_session
-    
+
     async def deactivate_current_session(self, refresh_token: str) -> Session | None:
         current_session = await self._session_repository.get_by_refresh_token(refresh_token)
         current_session.is_active = False
         updated_session = await self._session_repository.update(current_session)
         return updated_session
-    
+
     async def deactivate_all_without_current(self, refresh_token: str) -> list[Session]:
         current_session = await self._session_repository.get_by_refresh_token(refresh_token)
         user_sessions = await self._session_repository.get_sessions_by_user_id(current_session.user_id)
@@ -28,3 +32,9 @@ class SessionService(AbstractSessionService):
             deactivate_sessions.append(session)
         return deactivate_sessions
 
+
+def get_session_service(
+    session_repository: AbstractSessionRepository = Depends(get_session_repository),
+) -> AbstractSessionService:
+    session_service = SessionService(session_repository=session_repository)
+    return session_service
