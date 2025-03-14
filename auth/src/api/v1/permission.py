c055577f84b34fdaf7f03726fb09ccb11a6e3c86
suvorovjr <pglavan1998@mail.ru>
2025-03-14 13:27:47 +0300

    Add changes in permissions routes

--- auth/src/api/v1/permission.py
+++ auth/src/api/v1/permission.py
@@ -7,29 +7,34 @@ perm_router = APIRouter()
 
 
 @perm_router.get("/", response_model=list[PermissionResponse])
-async def get_all_permissions(permisson_service: PermissionService = Depends(get_permission_service)):
-    return permisson_service.get()
+async def get_all_permissions(
+    permission_service: PermissionService = Depends(get_permission_service),
+):
+    return await permission_service.get()
 
 
 @perm_router.get("/{slug}/", response_model=[PermissionResponse])
-async def get_permission(slug: str, permisson_service: PermissionService = Depends(get_permission_service)):
-    return permisson_service.get(slug=slug)
+async def get_permission(slug: str, permission_service: PermissionService = Depends(get_permission_service)):
+    return await permission_service.get(slug=slug)
 
 
 @perm_router.post("/", response_model=PermissionResponse)
 async def create_permission(
-    data: PermissionCreate, permisson_service: PermissionService = Depends(get_permission_service)
+    data: PermissionCreate,
+    permission_service: PermissionService = Depends(get_permission_service),
 ):
-    return permisson_service.create_or_update(data=data)
+    return await permission_service.create_or_update(data=data)
 
 
 @perm_router.patch("/{slug}/", response_model=PermissionResponse)
 async def change_permissions(
-    slug: str, data: PermissionCreate, permisson_service: PermissionService = Depends(get_permission_service)
+    slug: str,
+    data: PermissionCreate,
+    permission_service: PermissionService = Depends(get_permission_service),
 ):
-    return permisson_service.create_or_update(data=data, slug=slug)
+    return await permission_service.create_or_update(data=data, slug=slug)
 
 
 @perm_router.delete("/{slug}/", response_model=bool)
-async def delete_permission(slug: str, permisson_service: PermissionService = Depends(get_permission_service)):
-    return permisson_service.delete(slug=slug)
+async def delete_permission(slug: str, permission_service: PermissionService = Depends(get_permission_service)):
+    return await permission_service.delete(slug=slug)
