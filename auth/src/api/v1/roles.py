2d5c1d5b6720d267fc5047084c58d2a4fc545616
suvorovjr <pglavan1998@mail.ru>
2025-03-14 13:26:45 +0300

    Add changes in roles routes

--- auth/src/api/v1/roles.py
+++ auth/src/api/v1/roles.py
@@ -1,33 +1,35 @@
 from fastapi import APIRouter, Depends
 
 from src.api.v1.schemas.permissions import PermissionCreate, PermissionResponse
-from src.services.permission import PermissionService, get_permission_service
+from src.services.role import RoleService, get_role_service
 
-roles_rouner = APIRouter()
-
-
-@roles_rouner.get("/", response_model=list[PermissionResponse])
-async def get_all_roles(permisson_service: PermissionService = Depends(get_permission_service)):
-    return permisson_service.get()
+roles_router = APIRouter()
 
 
-@roles_rouner.get("/{slug}/", response_model=[PermissionResponse])
-async def get_role(slug: str, permisson_service: PermissionService = Depends(get_permission_service)):
-    return permisson_service.get(slug=slug)
+@roles_router.get("/", response_model=list[PermissionResponse])
+async def get_all_roles(role_service: RoleService = Depends(get_role_service)):
+    return await role_service.get()
 
 
-@roles_rouner.post("/", response_model=PermissionResponse)
-async def create_role(data: PermissionCreate, permisson_service: PermissionService = Depends(get_permission_service)):
-    return permisson_service.create_or_update(data=data)
+@roles_router.get("/{slug}/", response_model=[PermissionResponse])
+async def get_role(slug: str, permisson_service: RoleService = Depends(get_role_service)):
+    return await permisson_service.get(slug=slug)
 
 
-@roles_rouner.patch("/{slug}/", response_model=PermissionResponse)
+@roles_router.post("/", response_model=PermissionResponse)
+async def create_role(data: PermissionCreate, role_service: RoleService = Depends(get_role_service)):
+    return await role_service.create_or_update(data=data)
+
+
+@roles_router.patch("/{slug}/", response_model=PermissionResponse)
 async def change_role(
-    slug: str, data: PermissionCreate, permisson_service: PermissionService = Depends(get_permission_service)
+    slug: str,
+    data: PermissionCreate,
+    role_service: RoleService = Depends(get_role_service),
 ):
-    return permisson_service.create_or_update(data=data, slug=slug)
+    return await role_service.create_or_update(data=data, slug=slug)
 
 
-@roles_rouner.delete("/{slug}/", response_model=bool)
-async def delete_role(slug: str, permisson_service: PermissionService = Depends(get_permission_service)):
-    return permisson_service.delete(slug=slug)
+@roles_router.delete("/{slug}/", response_model=bool)
+async def delete_role(slug: str, role_service: RoleService = Depends(get_role_service)):
+    return await role_service.delete(slug=slug)
