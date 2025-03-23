7f09c0e2a4a2d95213e82364233145836df741f0
suvorovjr <pglavan1998@mail.ru>
2025-03-23 12:53:58 +0300

    Add Requiest id middleware

--- auth/src/core/middlewares.py
+++ auth/src/core/middlewares.py
@@ -46,3 +46,21 @@ class RateLimiterMiddleware(BaseHTTPMiddleware):
             pass
 
         return await call_next(request)
+
+
+class RequestIdMiddleware(BaseHTTPMiddleware):
+    """Проверяет наличие заголовка X-Request-Id для трасировки запросов"""
+
+    def __init__(self, app: ASGIApp):
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next: Callable):
+        request_id = request.headers.get("X-Request-Id")
+        if request_id is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"detail": "X-Request-Id is required"},
+            )
+
+        response = await call_next(request)
+        return response
