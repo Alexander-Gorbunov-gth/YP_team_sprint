8b31badbda32e33a23188f7c8f92d713d555807a
Pavel Glavan <pglavan1998@mail.ru>
2025-03-31 17:29:26 +0300

    Add event service

--- analitycal_service/src/services/event.py
+++ analitycal_service/src/services/event.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 
 from src.core.config import settings
 from src.domain.entities import ClientEvent
-from src.infrastructure.producer import AbstractProducerBroker, get_producer
+from src.infrastructure.brocker import AbstractProducerBroker, get_brocker
 
 
 class AbstractEventService(ABC):
@@ -16,17 +16,19 @@ class AbstractEventService(ABC):
 
 
 class EventService(AbstractEventService):
-    def __init__(self, producer: AbstractProducerBroker) -> None:
-        self._producer = producer
+    def __init__(self, brocker: AbstractProducerBroker) -> None:
+        self._brocker = brocker
 
     async def handle_event(self, event: ClientEvent, event_type: str) -> None:
-        await self._producer.send_message(topic=settings.producer.topic_name, value=event.model_dump(), key=event_type)
+        await self._brocker.send_message(
+            topic=settings.brocker.topic_name, value=event.model_dump_json(), key=event_type
+        )
 
     async def handle_event_and_wait(self, event: ClientEvent, event_type: str) -> None:
-        await self._producer.send_message_and_wait(
-            topic=settings.producer.topic_name, value=event.model_dump(), key=event_type
+        await self._brocker.send_message_and_wait(
+            topic=settings.brocker.topic_name, value=event.model_dump(), key=event_type
         )
 
 
-def get_event_service(producer: AbstractProducerBroker = Depends(get_producer)):
-    return EventService(producer=producer)
+def get_event_service(brocker: AbstractProducerBroker = Depends(get_brocker)):
+    return EventService(brocker=brocker)
