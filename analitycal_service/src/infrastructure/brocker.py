2c5c9da600b4262285b386c41877f5457704a638
Pavel Glavan <pglavan1998@mail.ru>
2025-03-31 17:29:54 +0300

    Add producer class

--- analitycal_service/src/infrastructure/brocker.py
+++ analitycal_service/src/infrastructure/brocker.py
@@ -22,36 +22,32 @@ class AbstractProducerBroker(ABC):
 class KafkaProducerWrapper(AbstractProducerBroker):
     def __init__(self, bootstrap_servers: str, username: str, password: str) -> None:
         """Инициализируем AIOKafkaProducer с авторизацией и сериализацией"""
-        self._producer = AIOKafkaProducer(
+        self._brocker = AIOKafkaProducer(
             bootstrap_servers=bootstrap_servers,
-            security_protocol="SASL_PLAINTEXT",
-            sasl_mechanism="PLAIN",
-            sasl_plain_username=username,
-            sasl_plain_password=password,
-            value_serializer=lambda v: json.dumps(v).encode("utf-8"),  # сериализация dict в JSON
-            key_serializer=lambda v: v.encode("utf-8"),  # сериализация ключа (строка → байты)
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            key_serializer=lambda v: v.encode("utf-8"),
         )
 
     async def start(self):
         """Устанавливаем соединение с Kafka и запускаем фоновые задачи продюсера"""
-        await self._producer.start()
+        await self._brocker.start()
 
     async def stop(self):
         """Завершаем соединение и останавливаем фоновые задачи"""
-        await self._producer.stop()
+        await self._brocker.stop()
 
     async def send_message(self, topic: str, value: dict, key: str | None = None):
         """Отправляем сообщение в Kafka (асинхронно, без ожидания подтверждения)"""
-        await self._producer.send(topic=topic, value=value, key=key)
+        await self._brocker.send(topic=topic, value=value, key=key)
 
     async def send_message_and_wait(self, topic: str, value: dict, key: str | None = None):
         """Отправляем сообщение и дожидаемся подтверждения от брокера"""
-        await self._producer.send_and_wait(topic=topic, value=value, key=key)
+        await self._brocker.send_and_wait(topic=topic, value=value, key=key)
 
 
 # Глобальный экземпляр — для внедрения DI
-producer: AbstractProducerBroker | None = None
+brocker: AbstractProducerBroker | None = None
 
 
-def get_producer() -> Optional[AbstractProducerBroker]:
-    return producer
+def get_brocker() -> AbstractProducerBroker:
+    return brocker
