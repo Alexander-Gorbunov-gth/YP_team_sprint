e39e7b4a97fb480c7074a66e4338e6612f8766dc
Pavel Glavan <pglavan1998@mail.ru>
2025-03-31 17:28:31 +0300

    Add domain entytites

--- analitycal_service/src/domain/entities.py
+++ analitycal_service/src/domain/entities.py
@@ -5,10 +5,51 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 
+class ClickPayload(BaseModel):
+    item_id: str
+    item_type: str
+
+
+class PageViewPayload(BaseModel):
+    page_type: str
+    duration_seconds: int
+
+
+class QualityChangePayload(BaseModel):
+    video_id: str
+    from_quality: str
+    to_quality: str
+    current_time_seconds: int
+
+
+class VideoFinishedPayload(BaseModel):
+    video_id: str
+    total_duration_seconds: int
+
+
+class SearchFilterPayload(BaseModel):
+    filters: list[str]
+
+
+class VideoProgressPayload(BaseModel):
+    video_id: str
+    watched_seconds: int
+
+
 class ClientEvent(BaseModel):
     user_id: UUID | str
     user_ip: str
     user_agent: str
-    event_type: Literal["click"]
-    timestamp: datetime = Field(default_factory=datetime.now())
+    event_type: Literal[
+        "click", "page_view", "quality_change", "video_finished", "search_filter_used", "video_progress"
+    ]
+    timestamp: datetime = Field(default_factory=datetime.now)
     payload: dict[str, str]
+    payload: (
+        ClickPayload
+        | PageViewPayload
+        | QualityChangePayload
+        | VideoFinishedPayload
+        | SearchFilterPayload
+        | VideoProgressPayload
+    )
