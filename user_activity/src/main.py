65c8b0dd1550dfdc184232a473aca69d2d6485d8
Pavel Glavan <pglavan1998@mail.ru>
2025-06-06 01:34:08 +0300

    Add init models

--- user_activity/src/main.py
+++ user_activity/src/main.py
@@ -1,13 +1,23 @@
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
+from beanie import init_beanie
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.core.config import settings
+from src.infrastructure import db
+from src.infrastructure.models import BookmarksModel, LikesModel, ReviewsModel
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncGenerator[None]:
+    db.mongo_client = AsyncIOMotorClient(settings.mongo.connection_url)
+    await init_beanie(
+        database=db.mongo_client[settings.mongo.db_name],
+        document_models=[LikesModel, BookmarksModel, ReviewsModel],
+    )
+
     yield
 
 
