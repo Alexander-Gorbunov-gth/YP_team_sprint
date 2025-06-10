6bc784a71e7e86f32a6f2cb413887159e7740753
Pavel Glavan <pglavan1998@mail.ru>
2025-06-10 23:15:15 +0300

    Add changes in models project

--- user_activity/src/infrastructure/models.py
+++ user_activity/src/infrastructure/models.py
@@ -10,27 +10,27 @@ class TimestampMixin(BaseModel):
     updated_at: datetime = Field(default_factory=datetime.now)
 
 
-class LikesModel(Document, TimestampMixin):
-    movie_uuid: UUID
-    user_uuid: UUID
+class LikeModel(Document, TimestampMixin):
+    movie_uid: UUID
+    user_uid: UUID
 
     class Settings:
-        name = "likes"
+        name = "like"
 
 
-class BookmarksModel(Document, TimestampMixin):
-    movie_uuid: UUID
-    user_uuid: UUID
+class BookmarkModel(Document, TimestampMixin):
+    movie_uid: UUID
+    user_uid: UUID
 
     class Settings:
-        name = "bookmarks"
+        name = "bookmark"
 
 
-class ReviewsModel(Document, TimestampMixin):
-    movie_id: UUID = Field(..., description="ID фильма")
-    user_id: UUID = Field(..., description="ID пользователя")
+class ReviewModel(Document, TimestampMixin):
+    movie_uid: UUID = Field(..., description="ID фильма")
+    user_uid: UUID = Field(..., description="ID пользователя")
     rating: int = Field(..., ge=1, le=10, description="Оценка фильма (1–10)")
     content: str = Field(..., description="Тело рецензии")
 
     class Settings:
-        name = "reviews"
+        name = "review"
