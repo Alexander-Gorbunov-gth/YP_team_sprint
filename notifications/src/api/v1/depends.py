662d6f6f1ca19520cf1e5dc7d72e5cb246831251
Pavel Glavan <pglavan1998@mail.ru>
2025-07-02 22:04:34 +0300

    Add websocket endpoint in project

--- notifications/src/api/v1/depends.py
+++ notifications/src/api/v1/depends.py
@@ -1,6 +1,11 @@
 from typing import Annotated
 
-from fastapi import Depends
+from circuitbreaker import circuit
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
+from httpx import AsyncClient, HTTPStatusError
+from src.core.config import settings
+from src.infrastructure.container import AppContainer
 from src.services.depends import Template_Repository_Dep
 from src.services.template import TemplateService
 
@@ -10,3 +15,21 @@ def get_template_service(template_repository: Template_Repository_Dep) -> Templa
 
 
 Template_Service_Dep = Annotated[TemplateService, Depends(get_template_service)]
+Http_Client_Dep = Annotated[AsyncClient, Depends(AppContainer.get_http_client)]
+Token_Dep = Annotated[str, HTTPBearer()]
+
+
+@circuit(failure_threshold=3, recovery_timeout=10)
+async def get_current_user(http_client: Http_Client_Dep, token: Token_Dep):
+    url = settings.auth.service_url
+    headers = {"Authorization": f"Bearer {token}"}
+    try:
+        async with http_client as client:
+            response = await client.get(url, headers=headers)
+            response.raise_for_status()
+            return await response.json()
+    except HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
+
+
+Current_User_Dep = Annotated[dict, Depends(get_current_user)]
--- notifications/src/api/v1/endpoints/websockets.py
+++ notifications/src/api/v1/endpoints/websockets.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, Query, WebSocket, WebSocketDisconnect
+from src.api.v1.depends import Current_User_Dep
 
 router = APIRouter(prefix="/ws", tags=["Websockets"])
 
@@ -9,11 +10,13 @@ active_connections: dict[UUID, WebSocket] = {}
 
 
 @router.websocket("/")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, current_user: Current_User_Dep):
+    """Websocket для отправки сообщений"""
+
     await websocket.accept()
-    active_connections[websocket.client_id] = websocket
+    active_connections[current_user["sub"]] = websocket
     try:
         while True:
             await websocket.receive_text()
     except WebSocketDisconnect:
-        del active_connections[websocket.client_id]
+        del active_connections[current_user["sub"]]
