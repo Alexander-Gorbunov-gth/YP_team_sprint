33a29d3a5c395d1a06dd85ea0dcd7fbacdc7eac7
Alexandr Gorbunov <gorbunov-sochi@yandex.ru>
2025-07-16 00:15:31 +0300

    booking

---
+++ booking_service/src/main.py
@@ -0,0 +1,42 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from httpx import AsyncClient
+from src.api.router import router as api_router
+from src.core.config import settings
+from src.infrastructure import db
+from src.infrastructure.clients import http
+
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncGenerator[None]:
+    
+    http.httpx_client = AsyncClient(timeout=5.0)
+
+    yield
+
+    await http.httpx_client.aclose()
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title=settings.proect.title,
+        description=settings.proect.decription,
+        debug=settings.proect.debug,
+        lifespan=lifespan,
+        docs_url="/api/openapi",
+        openapi_url="/api/openapi.json",
+        response_class=ORJSONResponse,
+    )
+    app.include_router(api_router)
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app()
