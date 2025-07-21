7a418c9a8f8309cdc8ad0cdb11d53a5af9d1340d
Pavel Glavan <pglavan1998@mail.ru>
2025-07-21 22:18:44 +0300

    Add main file in project

---
+++ booking_service/src/main.py
@@ -0,0 +1,16 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8000, reload=True)
