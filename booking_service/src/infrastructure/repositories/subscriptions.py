23b8f5c538be41ea862eeed4ddc8634ceed33c30
Pavel Glavan <pglavan1998@mail.ru>
2025-07-26 15:49:39 +0300

    Add changes in subscription repo

--- booking_service/src/infrastructure/repositories/subscriptions.py
+++ booking_service/src/infrastructure/repositories/subscriptions.py
@@ -7,6 +7,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.postgres import get_session
 from src.domain.entities.subscription import Subscription
+from src.domain.schemas.subscription import CreateSubscriptionSchema, DeleteSubscriptionSchema
+from src.infrastructure.repositories.exceptions import SubscriptionAlreadyExistsError, SubscriptionNotFoundError
 from src.services.interfaces.repositories.subscription import ISubscriptionRepository
 
 logger = logging.getLogger(__name__)
@@ -16,53 +18,52 @@ class SQLAlchemySubscriptionRepository(ISubscriptionRepository):
     def __init__(self, session: AsyncSession):
         self._session: AsyncSession = session
 
-    async def create(
-        self,
-        host_id: UUID,
-        user_id: UUID,
-        # TODO: Применить схему SubscriptionCreateSchema
-    ) -> Subscription:
-        check_query = select(Subscription).filter_by(host_id=host_id, user_id=user_id)
-        existing: Result = await self._session.execute(check_query)
-        subscription = existing.scalar_one_or_none()
+    async def create(self, subscription: CreateSubscriptionSchema) -> Subscription:
+        """
+        Создает подписку в базе данных.
+        :param subscription: Схема подписки для создания.
+        :raises SubscriptionAlreadyExistsError: Если подписка уже существует.
+        :return: Созданная подписка.
+        """
 
-        if subscription:
-            logger.warning(f"Добавляемый Subscription с host_id={host_id}, " + f"user_id={user_id} уже существует.")
-            return subscription
+        check_query = select(Subscription).filter_by(host_id=subscription.host_id, user_id=subscription.user_id)
+        existing: Result = await self._session.execute(check_query).scalar_one_or_none()
+        if existing is not None:
+            raise SubscriptionAlreadyExistsError(f"Подписка {subscription.host_id=} {subscription.user_id=} уже существует")
 
-        query = insert(Subscription).values({"host_id": host_id, "user_id": user_id}).returning(Subscription)
+        query = insert(Subscription).values(subscription.model_dump()).returning(Subscription)
         result: Result = await self._session.execute(query)
-        await self._commit()
         return result.scalar_one()
 
-    async def get_subscriptions_by_user_id(
-        self,
-        user_id: UUID | str,
-    ) -> list[Subscription]:
-        query = select(Subscription).filter_by(user_id=user_id)
+    async def delete(self, subscription: DeleteSubscriptionSchema) -> None:
+        """
+        Удаляет подписку из базы данных.
+        :param subscription: Схема подписки для удаления.
+        :raises SubscriptionNotFoundError: Если подписка не найдена.
+        """
+
+        check_query = select(Subscription).filter_by(host_id=subscription.host_id, user_id=subscription.user_id)
+        existing: Result = await self._session.execute(check_query).scalar_one_or_none()
+        if existing is None:
+            raise SubscriptionNotFoundError(f"Подписка с {subscription.host_id=} и {subscription.user_id=} не найдена.")
+        await self._session.delete(subscription)
+
+    async def get_subscriptions_by_user_id(self, user_id: UUID | str, limit: int, offset: int) -> list[Subscription]:
+        """
+        Получает все подписки для пользователя.
+        :param user_id: ID пользователя.
+        :return: Список подписок.
+        """
+
+        query = (
+            select(Subscription)
+            .filter_by(user_id=user_id)
+            .limit(limit)
+            .offset(offset)
+            .order_by(Subscription.created_at.desc())
+        )
         result: Result = await self._session.execute(query)
         return result.unique().scalars().all()
-
-    async def delete(
-        self,
-        host_id: UUID,
-        user_id: UUID,
-        # TODO: Применить схему SubscriptionDeleteSchema
-    ) -> Subscription | None:
-        query = select(Subscription).filter_by(user_id=user_id, host_id=host_id)
-        result: Result = await self._session.execute(query)
-        subscription = result.scalar_one_or_none()
-
-        if subscription is None:
-            logger.warning(f"Удаляемый Subscription с user_id={user_id}, " + f"host_id={host_id} не найден.")
-            return None
-
-        await self._session.delete(subscription)
-        await self._commit()
-        return subscription
-
-    async def _commit(self) -> None:
-        await self._session.commit()
 
 
 async def get_subscription_repository(session: AsyncSession = Depends(get_session)) -> SQLAlchemySubscriptionRepository:
