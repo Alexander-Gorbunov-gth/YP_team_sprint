2e1882e3bca0de129cce33908b7221e48b58f3c5
Pavel Glavan <pglavan1998@mail.ru>
2025-07-26 23:37:10 +0300

    Add init containers in main file

--- booking_service/src/main.py
+++ booking_service/src/main.py
@@ -1,18 +1,35 @@
 from contextlib import asynccontextmanager
 
 import uvicorn
+from dishka import make_async_container
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 
 from src.api.v1.router import router
+from src.infrastructure.container import Container
+from src.infrastructure.lifetime import AppLifetime
 
 
 @asynccontextmanager
-async def lifespan(_: FastAPI):
-    yield
+async def lifespan(app: FastAPI):
+    app_lifetime = AppLifetime()
+    await app_lifetime.startup()
+    try:
+        yield
+    finally:
+        await app_lifetime.shutdown()
+        await app.state.dishka_container.close()
 
-app = FastAPI(lifespan=lifespan)
 
-app.include_router(router, prefix="/api")
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan)
+    app.include_router(router, prefix="/api")
+    container = make_async_container(Container())
+    setup_dishka(container=container, app=app)
+    return app
+
+
+app = create_app()
 
 if __name__ == "__main__":
     uvicorn.run(app=app, host="0.0.0.0", port=8000, reload=True)
