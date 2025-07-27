8320ed0180e33eb60ee54bafd2849aa9d74e09b2
Pavel Glavan <pglavan1998@mail.ru>
2025-07-27 15:42:34 +0300

    Add changes in subscription endpoint

--- booking_service/src/api/v1/endpoints/subscribe.py
+++ booking_service/src/api/v1/endpoints/subscribe.py
@@ -1,65 +1,52 @@
 import logging
-from uuid import UUID
 
 from dishka.integrations.fastapi import DishkaRoute, FromDishka
 from fastapi import APIRouter, Query
 
-from src.domain.schemas.subscription import (
+from src.api.v1.depends import CurrentUserDep
+from src.api.v1.schemas.subscription import (
     SubscriptionCreateSchema,
     SubscriptionDeleteSchema,
-    SubscriptionRepresentSchema,
+    SubscriptionResponseSchema,
 )
+from src.domain.dtos.subscription import SubscriptionCreateDTO, SubscriptionDeleteDTO
 from src.services.subscription import ISubscriptionService
-from src.domain.schemas import subscription as schema
-from tests.unit.routers_fixture import sub_data1, sub_data2
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/subscribe", tags=["Subscribe"], route_class=DishkaRoute)
 
 
-
-@router.get("/my/", summary="Получить мои подписки", response_model=list[SubscriptionRepresentSchema])
+@router.get("/my/", summary="Получить мои подписки", response_model=list[SubscriptionResponseSchema])
 async def get_user_subscriptions(
     subscription_service: FromDishka[ISubscriptionService],
-    user_id: UUID | str,
+    user: CurrentUserDep,
     limit: int = Query(default=10, description="Количество подписок"),
     offset: int = Query(default=0, description="Сдвиг"),
 ):
-    subscriptions = await subscription_service.get_subscriptions_by_user_id(user_id, limit, offset)
-    return [SubscriptionRepresentSchema.model_validate(sub) for sub in subscriptions]
+    subscriptions = await subscription_service.get_subscriptions_by_user_id(user.id, limit, offset)
+    return [SubscriptionResponseSchema.model_validate(sub) for sub in subscriptions]
 
 
-@router.post("/", summary="Подписаться на автора", response_model=SubscriptionRepresentSchema)
+@router.post("/", summary="Подписаться на автора", response_model=SubscriptionResponseSchema)
 async def subscribe(
-    subscription_service: FromDishka[ISubscriptionService], user_id: UUID | str, host_id: UUID | str
-) -> SubscriptionRepresentSchema:
+    subscription_service: FromDishka[ISubscriptionService],
+    user: CurrentUserDep,
+    subscription_data: SubscriptionCreateSchema,
+) -> SubscriptionResponseSchema:
     subscription = await subscription_service.create_subscription(
-        SubscriptionCreateSchema(user_id=user_id, host_id=host_id)
+        SubscriptionCreateDTO(user_id=user.id, host_id=subscription_data.host_id)
     )
-
-@router.get(
-    "/my/",
-    summary="Получить мои подписки",
-    response_model=list[schema.SubscriptionRepresentSchema],
-)
-async def get_events():
-    return [sub_data1, sub_data2]
-
-
-@router.post(
-    "/",
-    summary="Подписаться на автора",
-    response_model=schema.SubscriptionRepresentSchema,
-)
-async def subscribe(data: schema.SubscriptionCreateSchema):
-    return sub_data1
-
+    return SubscriptionResponseSchema.model_validate(subscription)
 
 
 @router.delete("/", summary="Отписаться от автора")
 async def unsubscribe(
-    subscription_service: FromDishka[ISubscriptionService], user_id: UUID | str, host_id: UUID | str
+    subscription_service: FromDishka[ISubscriptionService],
+    user: CurrentUserDep,
+    subscription_data: SubscriptionDeleteSchema,
 ) -> bool:
-    await subscription_service.delete_subscription(SubscriptionDeleteSchema(user_id=user_id, host_id=host_id))
+    await subscription_service.delete_subscription(
+        SubscriptionDeleteDTO(user_id=user.id, host_id=subscription_data.host_id)
+    )
     return True
