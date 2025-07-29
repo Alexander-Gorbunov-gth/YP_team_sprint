15d79675599000f82cf97f3a78e3978dd4793b98
Pavel Glavan <pglavan1998@mail.ru>
2025-07-30 01:58:59 +0300

    Add changes in address repo

--- booking_service/tests/fakes/repositories/address.py
+++ booking_service/tests/fakes/repositories/address.py
@@ -1,9 +1,8 @@
 from collections.abc import Iterable
 from uuid import UUID
 
-from src.domain.dtos.address import AddressUpdateDTO, AdressCreateDTO
+from src.domain.dtos.address import AddressCreateDTO, AddressUpdateDTO
 from src.domain.entities.address import Address
-from src.infrastructure.repositories.exceptions import AddressNotFoundError, NotModifiedError
 from src.services.interfaces.repositories.address import IAddressRepository
 
 
@@ -11,29 +10,34 @@ class FakeAddressRepository(IAddressRepository):
     def __init__(self) -> None:
         self._addresses: list[Address] = []
 
-    async def create(self, address: AdressCreateDTO) -> Address:
+    async def create(self, address: AddressCreateDTO) -> Address:
         created_address = Address.create(**address.model_dump())
         self._addresses.append(created_address)
         return created_address
 
-    async def get_address(self, address_id: UUID) -> Address:
+    async def get_address(self, address_id: UUID) -> Address | None:
         for address in self._addresses:
             if address.id == address_id:
                 return address
-        raise AddressNotFoundError(f"Address с id={address_id} не найден.")
+        return None
 
     async def get_my_addresses(self, user_id: UUID) -> Iterable[Address]:
         return [address for address in self._addresses if address.user_id == user_id]
 
-    async def delete(self, address_id: UUID) -> None:
-        self._addresses = [address for address in self._addresses if address.id != address_id]
+    async def delete(self, address_id: UUID) -> Address | None:
+        for address in self._addresses:
+            if address.id == address_id:
+                self._addresses.remove(address)
+                return address
+        return None
 
-    async def update(self, address: AddressUpdateDTO) -> Address:
+    async def update(self, address: AddressUpdateDTO) -> Address | None:
         insert_data = address.model_dump(exclude_unset=True)
         if not insert_data:
-            raise NotModifiedError("Не указаны данные для обновления.")
+            return None
         for i, a in enumerate(self._addresses):
             if a.id == address.id:
-                self._addresses[i] = Address.create(**address.model_dump())
+                for key, value in insert_data.items():
+                    setattr(self._addresses[i], key, value)
                 return self._addresses[i]
-        raise AddressNotFoundError(f"Address с id={address.id} не найден.")
+        return None
