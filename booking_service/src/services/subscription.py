5e930771bedee6f2159465ca0205deed067c265e
Pavel Glavan <pglavan1998@mail.ru>
2025-07-29 21:11:01 +0300

    Add changes in subscriprion service

--- booking_service/src/services/subscription.py
+++ booking_service/src/services/subscription.py
@@ -1,18 +1,22 @@
 import abc
+import logging
 from collections.abc import Iterable
 from uuid import UUID
 
+from src.domain.dtos.subscription import SubscriptionCreateDTO, SubscriptionDeleteDTO
 from src.domain.entities.subscription import Subscription
-from src.domain.schemas.subscription import SubscriptionCreateSchema, SubscriptionDeleteSchema
+from src.infrastructure.repositories.exceptions import SubscriptionAlreadyExistsError, SubscriptionNotFoundError
 from src.services.interfaces.uow import IUnitOfWork
+
+logger = logging.getLogger(__name__)
 
 
 class ISubscriptionService(abc.ABC):
     @abc.abstractmethod
-    async def create_subscription(self, subscription: SubscriptionCreateSchema) -> Subscription: ...
+    async def create_subscription(self, subscription: SubscriptionCreateDTO) -> Subscription: ...
 
     @abc.abstractmethod
-    async def delete_subscription(self, subscription: SubscriptionDeleteSchema) -> None: ...
+    async def delete_subscription(self, subscription: SubscriptionDeleteDTO) -> None: ...
 
     @abc.abstractmethod
     async def get_subscriptions_by_user_id(
@@ -24,18 +28,21 @@ class SubscriptionService(ISubscriptionService):
     def __init__(self, uow: IUnitOfWork):
         self._uow = uow
 
-    async def create_subscription(self, subscription: SubscriptionCreateSchema) -> Subscription:
+    async def create_subscription(self, subscription: SubscriptionCreateDTO) -> Subscription:
         async with self._uow as uow:
+            created_subscription = await uow.subscription_repository.create(subscription)
+            if created_subscription is None:
+                logger.warning("Подписка с host_id=%s и user_id=%s уже существует", subscription.host_id, subscription.user_id)
+                raise SubscriptionAlreadyExistsError("Подписка уже существует")
             return Subscription.model_validate(await uow.subscription_repository.create(subscription))
 
-    async def delete_subscription(self, subscription: SubscriptionDeleteSchema) -> None:
+    async def delete_subscription(self, subscription: SubscriptionDeleteDTO) -> None:
         async with self._uow as uow:
-            await uow.subscription_repository.delete(subscription)
+            deleted_subscription = await uow.subscription_repository.delete(subscription)
+            if deleted_subscription is None:
+                logger.warning("Подписка с host_id=%s и user_id=%s не найдена", subscription.host_id, subscription.user_id)
+                raise SubscriptionNotFoundError("Подписка не найдена")
 
-    async def get_subscriptions_by_user_id(
-        self, user_id: UUID | str, limit: int, offset: int
-    ) -> Iterable[Subscription]:
+    async def get_subscriptions_by_user_id(self, user_id: UUID | str, limit: int, offset: int) -> Iterable[Subscription]:
         async with self._uow as uow:
-            return await uow.subscription_repository.get_subscriptions_by_user_id(
-                user_id=user_id, limit=limit, offset=offset
-            )
+            return await uow.subscription_repository.get_subscriptions_by_user_id(user_id=user_id, limit=limit, offset=offset)
