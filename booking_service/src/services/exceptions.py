ee8b750680ebd8f2fa495b930a271b87a4306ec2
Pavel Glavan <pglavan1998@mail.ru>
2025-07-30 02:14:23 +0300

    Add address exceptions

--- booking_service/src/services/exceptions.py
+++ booking_service/src/services/exceptions.py
@@ -8,3 +8,7 @@ class EventNotFoundError(Exception):
 
 class SessionHasExpired(Exception):
     """Исключение, которое выбрасывается, если сессия истекла."""
+
+
+class AddressNotFoundError(Exception):
+    """Исключение, которое выбрасывается, если адрес не найден."""
