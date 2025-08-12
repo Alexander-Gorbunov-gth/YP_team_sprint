f855e8612a7b3f94bafabee3351606fc848e15ab
Pavel Glavan <pglavan1998@mail.ru>
2025-08-12 06:58:16 +0300

    Add changes in tests

--- booking_service/tests/fakes/repositories/event.py
+++ booking_service/tests/fakes/repositories/event.py
@@ -1,6 +1,8 @@
+import asyncio
+from collections.abc import Sequence
 from uuid import UUID, uuid4
 
-from src.domain.dtos.event import EventCreateDTO, EventUpdateDTO
+from src.domain.dtos.event import EventCreateDTO, EventGetAllDTO, EventUpdateDTO
 from src.domain.entities.event import Event
 from src.services.interfaces.repositories.event import IEventRepository
 
@@ -8,6 +10,7 @@ from src.services.interfaces.repositories.event import IEventRepository
 class FakeEventRepository(IEventRepository):
     def __init__(self) -> None:
         self._events: list[Event] = []
+        self._lock = asyncio.Lock()
 
     async def create(self, event: EventCreateDTO) -> Event:
         created_event = Event(**event.model_dump(), id=uuid4())
@@ -33,5 +36,24 @@ class FakeEventRepository(IEventRepository):
     async def get_events_by_user_id(self, user_id: UUID | str) -> list[Event]:
         return [event for event in self._events if event.owner_id == user_id]
 
-    async def get_event_list(self, offset: int, limit: int) -> list[Event]:
-        return self._events[offset : offset + limit]
+    async def get_event_list(self, event: EventGetAllDTO) -> Sequence[Event]:
+        return self._events[event.offset : event.offset + event.limit]
+
+    async def get_events_by_addresses(self, addresses: list[UUID]) -> Sequence[Event]:
+        return [event for event in self._events if event.address_id in addresses]
+
+    async def get_for_update(self, event_id: UUID | str) -> Event | None:
+        await self._lock.acquire()  # Захватываем "строку"
+        try:
+            for event in self._events:
+                if event.id == event_id:
+                    return event
+            return None
+        finally:
+            pass
+
+    async def release_lock(self):
+        if self._lock:
+            self._lock.release()
+            self._lock = None
+
--- booking_service/tests/fakes/uow.py
+++ booking_service/tests/fakes/uow.py
@@ -1,6 +1,8 @@
 from src.services.interfaces.producer import IProducer
 from src.services.interfaces.repositories.address import IAddressRepository
 from src.services.interfaces.repositories.event import IEventRepository
+from src.services.interfaces.repositories.reservation import IReservationRepository
+from src.services.interfaces.repositories.feedback import IEventFeedbackRepository, IFeedbackRepository
 from src.services.interfaces.repositories.subscription import ISubscriptionRepository
 from src.services.interfaces.uow import IUnitOfWork
 from tests.fakes.producer import FakeProducer
@@ -15,6 +17,9 @@ class FakeUnitOfWork(IUnitOfWork):
         self._event_repository = FakeEventRepository()
         self._producer = FakeProducer()
         self._address_repository = FakeAddressRepository()
+        self._user_feedback_repository = None
+        self._event_feedback_repository = None
+        self._reservation_repository = None
 
     async def __aenter__(self) -> "FakeUnitOfWork":
         return self
@@ -23,10 +28,10 @@ class FakeUnitOfWork(IUnitOfWork):
         pass
 
     async def commit(self) -> None:
-        pass
+        await self._event_repository.release_lock()
 
     async def rollback(self) -> None:
-        pass
+        await self._event_repository.release_lock()
 
     @property
     def subscription_repository(self) -> ISubscriptionRepository:
@@ -43,3 +48,15 @@ class FakeUnitOfWork(IUnitOfWork):
     @property
     def producer(self) -> IProducer:
         return self._producer
+
+    @property
+    def reservation_repository(self) -> IReservationRepository:
+        return self._reservation_repository
+
+    @property
+    def user_feedback_repository(self) -> IFeedbackRepository:
+        return self._user_feedback_repository
+
+    @property
+    def event_feedback_repository(self) -> IEventFeedbackRepository:
+        return self._event_feedback_repository
